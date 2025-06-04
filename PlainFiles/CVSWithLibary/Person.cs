c40ed1f8a3877292dfb4e083223418b1e7668757
brayan barrera <brayanstbv@gmail.com>
2025-06-03 21:55:36 -0500

    create user

--- PlainFiles/CVSWithLibary/Person.cs
+++ PlainFiles/CVSWithLibary/Person.cs
@@ -12,6 +12,7 @@ public class Person
     public override string ToString()
     {
         return $"{Id}\t{FirstName} {LastName}\n\t" +
+
             $"Phone: {Phone}\n\t" +
             $"City: {City}\n\t" +
             $"Balance: {Balance,20:C2}\n";
---
+++ PlainFiles/CVSWithLibary/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVSWithLibary;
+
+public class User
+{
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public int FailedLoginAttempts { get; set; } = 0;
+
+    public override string ToString()
+    {
+        return $"Username: {Username}, Active: {IsActive}";
+    }
+}
